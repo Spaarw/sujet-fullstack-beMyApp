8642349e6c1da3c870f277c795e45a8ad223cd19
sparw <spaaarw@gmail.com>
2017-06-26 15:41:18 +0200

    Mise à jour client + Ajout d'un package.json client + Ajout d'un app.js

---
+++ www/app.js
@@ -0,0 +1,14 @@
+/**
+ * @author Pierre Petit
+ * @description APPLICATION CONFIGURATION
+ * @date 26/06/2017
+ * @copyright Rezocean
+ */
+
+"use strict";
+
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap']);
+
+app.run(['$rootScope'], function($rootScope) {
+	console.log('test');
+});
